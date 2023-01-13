22918927c48cc86891697f3da08cf176b10cf5c4
ChelseaDCF <122062141+ChelseaDCF@users.noreply.github.com>
2023-01-13 14:03:24 -0800

    Updates
    Changed file paths and text for new model deployment

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,14 @@ from PIL import ImageOps
 import pathlib
 
 def main():
-    st.title('Peru Fish Classifier')
+    st.title('Black Bear or Brown Bear?')
 
     for filename in EXTERNAL_DEPENDENCIES.keys():
         download_file(filename)
     
     model = load_model()
     
-    st.markdown("Fish photo for classification.")
+    st.markdown("Bear photo to indentify.")
     image = st.file_uploader("", IMAGE_TYPES)
     if image:
         image_data = image.read()
@@ -56,7 +56,7 @@ def load_model():
     plt = platform.system()
 
     if plt == 'Linux' or plt == 'Darwin': pathlib.WindowsPath = pathlib.PosixPath
-    inf_model = load_learner('perumixed3.pkl', cpu=True)
+    inf_model = load_learner('Example_Trail_1.pkl', cpu=True)
 
     return inf_model
 
@@ -103,9 +103,9 @@ def download_file(file_path):
 IMAGE_TYPES = ["png", "jpg"]
 
 EXTERNAL_DEPENDENCIES = {
-    "perumixed3.pkl": {
-        "url": "https://www.dropbox.com/s/31e6wuwrlm66sco/perumixed3.pkl?dl=1",
-        "size": 179319095
+    "Example_Trail_1.pkl": {
+        "url": "https://drive.google.com/file/d/1TYC38aI_B9Vo8gJVZ6bpPN9Hx_9uiKKt/view?usp=share_link",
+        "size": 179195904
     }
 }
 
